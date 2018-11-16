e1b107a570d01589c1bd205af105709eea928f3a
Olexandr <olexandr.maltsev@gmail.com>
2018-11-16 12:56:27 +0200

    adds static statements, fix table

--- src/main/java/MainStaticStatementCreate.java
+++ src/main/java/MainStaticStatementCreate.java
@@ -1,30 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class CreateConnection {
-
+public class MainStaticStatementCreate {
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String USER = "root";
     private static final String PASSWORD = "password";
 
     public static void main(String[] args) {
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD); Statement statement = connection.createStatement()) {
 
-        try {
-            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("Connection established");
-            }
-
-            connection.close();
-
-            if (connection.isClosed()) {
-                System.out.println("Connection closed");
-            }
+            statement.execute("INSERT INTO animal(anim_name, anim_desc) VALUES('name', 'desc');");
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
     }
 }
--- src/main/resources/createTable.sql
+++ src/main/resources/createTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `mydbtest`.`animal` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `animal_name` VARCHAR(45) NULL,
+  `anim_name` VARCHAR(45) NULL,
   `anim_desc` VARCHAR(255) NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `idanimal_UNIQUE` (`id` ASC) VISIBLE);
+  UNIQUE INDEX `idanimal_UNIQUE` (`id` ASC) );
