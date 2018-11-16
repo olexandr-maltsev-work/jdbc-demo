5c7b6b5aab57557ec445bc3c2514bd2e8c8b4219
Olexandr Maltsev <olexandr.maltsev@gmail.com>
2018-11-16 12:46:42 +0200

    adds table script

--- src/main/resources/createTable.sql
+++ src/main/resources/createTable.sql
@@ -1,1 +1,6 @@
-create database mydbtest;
+CREATE TABLE `mydbtest`.`animal` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `animal_name` VARCHAR(45) NULL,
+  `anim_desc` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `idanimal_UNIQUE` (`id` ASC) VISIBLE);
