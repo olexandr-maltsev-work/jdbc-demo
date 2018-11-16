c965e357cee8b5688112ba2b1d3d61e692e6ffd5
Olexandr <olexandr.maltsev@gmail.com>
2018-11-16 12:17:11 +0200

    refactoring

--- src/main/resources/demo_queries1.sql
+++ src/main/resources/demo_queries1.sql
@@ -1,6 +1,6 @@
-#insert into users(name, age, email) values('Steve', 55, 'steve@good.com');
+insert into users(name, age, email) values('Steve', 55, 'steve@good.com');
 
-select * from users;
+#select * from users;
 #select * from users where name = 'Steve';
 
 #update users set name='Steve', age = 55 where id = 4;
